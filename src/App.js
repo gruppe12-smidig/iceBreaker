bf3365703d3aacbe5c6119941f6a9b87503ac9ec
mathiasflaaten <mathiasbflaaten@gmail.com>
2019-05-21 11:15:55 +0200

    fixed routing

--- src/App.js
+++ src/App.js
@@ -1,29 +1,12 @@
 import React from 'react';
-import {Switch, Route} from 'react-router-dom';
 import './App.css';
 
-import {login} from "/login";
-import {registerUser} from "/registerUser";
-import {registerEvent} from "/registerEvent";
-import {menuPage} from "/menuPage";
-
-
-
-
-
-
-
 function App() {
-  return (
-    <div className="App">
-      <Switch>
-        <Route exact path="/login" component={login}/>
-        <Route exact path="/registerUser" component={registerUser}/>
-        <Route exact path="/registerEvent" component={registerEvent}/>
-        <Route exact path="/menuPage" component={menuPage}/>
-      </Switch>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1> HELLO </h1>
+        </div>
+    );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Route, Link, BrowserRouter as Router} from "react-router-dom";
 import './index.css';
 import App from './App';
+import Login from './login';
+import MenuPage from './menuPage';
+import RegisterUser from './registerUser';
+import RegisterEvent from './registerEvent'
 import * as serviceWorker from './serviceWorker';
 import * as firebase from 'firebase';
+
+const routing = (
+    <Router>
+        <div>
+            <Route path="/" component={App} />
+            <Route path="/login" component={Login} />
+            <Route path="/menuPage" component={MenuPage} />
+            <Route path="/registerUser" component={RegisterUser}/>
+            <Route path="/registerEvent" component={RegisterEvent}/>
+        </div>
+    </Router>
+)
 
 const firebaseConfig = {
     apiKey: "AIzaSyA9PZxcM-47HTb1X4MX6tJdlCyz50T4H5w",
@@ -16,7 +33,7 @@ const firebaseConfig = {
 };
 firebase.initializeApp(firebaseConfig);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(routing, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
