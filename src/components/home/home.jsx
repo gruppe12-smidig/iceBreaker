69b219339fe91058cca918c1fe69afa0e775958e
Linda <lindae.osterberg@gmail.com>
2019-05-23 19:44:47 +0200

    homepage penguin

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,14 +1,22 @@
 import React, {Component} from 'react';
+import './Home.css';
 import Header from "../header/Header";
 import Footer from "../footer/Footer";
+import penguin from '../../images/penguin.png';
 
 class Home extends Component {
     render() {
         return (
             <div>
                 <Header/>
-                <div id='HomeContainer'>
-                    <h3>Icebreaker</h3>
+                <div className='homeContainer'>
+                 
+                    <div className='wrapperPenguin'>
+                            <img className='frontPenguin' src={penguin} alt="Image of penguin" />
+                            <img className='frontPenguin' src={penguin} alt="Image of penguin" />
+                    </div>
+                    <h3>Break som Ice</h3>
+
                     <div className='button_wrapper'>
                         <button className='btn'>Find events</button>
                         <button className='btn'>My events</button>
