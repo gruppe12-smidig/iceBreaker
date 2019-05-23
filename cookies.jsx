f6f72a0d0f88310e573ff0d07e05fe01b47e3ee4
Sebastian <31272793+Slaskenx@users.noreply.github.com>
2019-05-23 12:48:48 +0200

    Create cookies.jsx

---
+++ cookies.jsx
@@ -0,0 +1,24 @@
+<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/cookieconsent@3/build/cookieconsent.min.css" />
+        <script src="https://cdn.jsdelivr.net/npm/cookieconsent@3/build/cookieconsent.min.js"></script>
+            <script>
+                window.addEventListener("load", function(){
+                window.cookieconsent.initialise({
+                    "palette": {
+                        "popup": {
+                            "background": "#252e39"
+                        },
+                        "button": {
+                            "background": "#14a7d0"
+                        }
+                    },
+                    "theme": "edgeless",
+                    "content": {
+                        "href": "https://nettvett.no/slik-administrer-du-informasjonskapsler/"
+                    }
+                })});
+            </script>
+            <!--
+                Using Osano's Cookie Consent template to create a script that loads 
+                a pop-up banner to inform the user that we use cookies and a link
+                to learn more about cookies on nettvett.no
+            -->
