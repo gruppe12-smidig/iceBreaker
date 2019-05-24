338698f673aa4bac1bdfc9c00963428f929df8e5
Karolis <karolis.pt@gmail.com>
2019-05-24 15:42:46 +0200

    header update

--- src/components/App.js
+++ src/components/App.js
@@ -7,21 +7,56 @@ import LoginPage from './login/LoginPage';
 import RegisterEventPage from './event/RegisterEventPage';
 import SignupPage from './signup/SignupPage';
 import Events from './events/Events';
+import Switch from "react-router-dom/es/Switch";
+import Header from "./header/Header";
+import Backdrop from "./Backdrop";
+import SideDrawer from "./SideDrawer";
 
 
 class App extends Component {
- render() {
-     return (
-         <Router>
-                 <Route exact path="/" component={Home}/>
-                 <Route path="/loginPage" component={LoginPage}/>
-                 <Route path="/SignupPage" component={SignupPage}/>
-                 <Route path="/RegisterEventPage" component={RegisterEventPage}/>
-                 <Route path="/Events" component={Events}/>
-         </Router>
-     );
- }
+    state = {
+        sideDrawerOpen: false
+    };
+
+    drawerToggleClickHandler = () => {
+        this.setState((prevState) => {
+            return {sideDrawerOpen: !prevState.sideDrawerOpen};
+        });
+    };
+
+
+    backDropClickHandler = () => {
+        this.setState({sideDrawerOpen: false});
+    };
+
+    render() {
+        let sideDrawer;
+        let backdrop;
+
+        if (this.state.sideDrawerOpen) {
+            sideDrawer = <SideDrawer/>;
+            backdrop = <Backdrop click={this.backDropClickHandler}/>;
+        }
+
+        return (
+            <div style={{height: '100%'}}>
+
+                <Router>
+                    <Header drawerClickHandler={this.drawerToggleClickHandler}/>
+                    {sideDrawer}
+                    {backdrop}
+
+                    <Switch>
+                        <Route exact path="/" component={Home}/>
+                        <Route path="/loginPage" component={LoginPage}/>
+                        <Route path="/SignupPage" component={SignupPage}/>
+                        <Route path="/RegisterEventPage" component={RegisterEventPage}/>
+                    </Switch>
+                </Router>
+            </div>
+        );
+    }
 }
 
-
 export default App;
+
