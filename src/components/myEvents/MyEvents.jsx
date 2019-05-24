ab2416e54f5da4c92eaee0fb1210fdb8040663c1
Linda <lindae.osterberg@gmail.com>
2019-05-25 01:30:37 +0200

    My events basci done

--- src/components/myEvents/MyEvents.jsx
+++ src/components/myEvents/MyEvents.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import './MyEvents.css'; 
+import './MyEvents.css';
+import Footer from "../footer/Footer";
 import Header from '../header/Header';
 
 
@@ -8,24 +9,67 @@ import Header from '../header/Header';
 
 class MyEvents extends Component {
     state = {
- 
+
     }
 
-   
+
     render() {
 
-        return(
+        return (
 
-            <div className="mainContainer">
-                          
+         <div>
+                    <div className="mainContainer">
 
-                         <h2 className='subHeader' >mainContainer arrangementer</h2>
+
+                        <h2 className='subHeader' >Mine arrangementer</h2>
+
+                        <div className='eventContainer'>
+
+                            <div className='adminEventsContainer'>
+                                <h3 className='subHeader'>Ansvarlig for</h3>
+                                {/*Render of event components from DB  */}
+                                <ul>
+
+                                    <li className='eventBox'><div className="testEvent"></div></li>
+                                    <li className='eventBox'><div className="testEvent"></div></li>
+                                    <li className='eventBox'><div className="testEvent"></div></li>
+
+                                </ul>
+
+                                <a href="/RegisterEventPage"><button className='btn'>Opprett ny</button></a> 
+
+                            </div>
+
+                            <div className="joinedEventsContainer" >
+
+                                <h3 className='subHeader'>Oppmeldt til</h3>
+                                {/*Render of event components from DB  */}
+                                <ul>
+                                <li className='eventBox'><div className="testEvent"></div></li>
+                                <li className='eventBox'><div className="testEvent"></div></li>
+                                <li className='eventBox'><div className="testEvent"></div></li>
+                                <li className='eventBox'><div className="testEvent"></div></li>
+                                <li className='eventBox'><div className="testEvent"></div></li>
+
+                                </ul>
+
+                                <a href="/Events"><button className='btn'>Finn ny</button></a> 
+
+                            </div>
+
+
+
+
+
+
+                        </div>
+
                 
-                <div className='eventContainer'>        
+                    </div>
 
-                </div>
+            <Footer/> 
 
-            </div>
+            </div>     
         )
     }
 }
