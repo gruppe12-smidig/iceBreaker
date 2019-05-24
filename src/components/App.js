9b48fd8ebd357aceeccf2ec98d7fc0a2918e94d6
Karolis <karolis.pt@gmail.com>
2019-05-24 15:13:28 +0200

    header update

--- src/components/App.js
+++ src/components/App.js
@@ -44,7 +44,6 @@ class App extends Component {
                     <Header drawerClickHandler={this.drawerToggleClickHandler}/>
                     {sideDrawer}
                     {backdrop}
-
                     <Switch>
                         <Route exact path="/" component={Home}/>
                         <Route path="/loginPage" component={LoginPage}/>
