b108b011cbb725d42b420a9567a708f7bd9c6795
Karolis <karolis.pt@gmail.com>
2019-05-26 16:20:41 +0200

    little update

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,10 +1,13 @@
 import React, {Component} from 'react';
 import './About.css'
+import Footer from '../footer/Footer.jsx'
 
 class About extends Component {
     render() {
         return (
             <div>
+                <h1>OM OSS</h1>
+                <Footer />
 
             </div>
         );
