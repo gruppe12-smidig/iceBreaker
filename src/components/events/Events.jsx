3c71310fb7e194c20cdb4dd7ddf6c1010738e024
Elsa Mari Kristensen <elsamari_1998@live.no>
2019-05-26 17:13:17 +0200

    b

--- src/components/events/Events.jsx
+++ src/components/events/Events.jsx
@@ -1,8 +1,8 @@
-
 import React, { Component } from 'react';
 import './Events.css';
 import Header from '../header/Header';
-
+import Footer from "../footer/Footer";
+import boot from "../../images/boot.png";
 
 
 class Events extends Component {
@@ -11,20 +11,23 @@ state = {
 render() {
 return(
 <div>
-<Header />
-<h2 className='page-header'>Arrangementer</h2>
-<div id="wrapperButtons">
-<button className='btn-coffeMeeting'>Kaffetreff<img className="tea" src={tea} alt="tea"/> </button> 
-<button className='btn-onTour'>Ut på tur<img className="boot" src={boot} alt="boot"/></button>
-<button className='btn-physicalActivity'>Fysisk aktivitet</button>
-<button className='btn-foodAndTalk'>Mat&Prat</button>
-<button className='btn-studyGroup'>Studiegrupper</button>
+    <Header />
+    <h2 className='page-header'>Arrangementer</h2>
+    <div id="wrapperButtons">
+        <button className='btn-coffeMeeting'>Kaffetreff</button>
+        <button className='btn-onTour'>Ut på tur</button>
+        <button className='btn-physicalActivity'>Fysisk aktivitet</button>
+        <button className='btn-foodAndTalk'>Mat&Prat</button>
+        <button className='btn-studyGroup'>Studiegrupper</button>
 
-<button className='btn-test'>Test
-<img className="tea" src={tea} alt="tea"/>
-<span className="btn-test1">TEST</span>
-</button>
+        <button className="btn-test">
+        <span className="Test">Test</span>
+        <img className='boot' src={boot} alt="boot"/>
+        </button>
 
+    </div>
+    <Footer />
+</div>
 )
 }
 }
