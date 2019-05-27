2a6696999349b887a7c3f1550aabc1e9dce6e673
Elsa Mari Kristensen <elsamari_1998@live.no>
2019-05-27 13:36:33 +0200

    j

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,17 +1,16 @@
 import React, {Component} from 'react';
 import './About.css'
 import Footer from '../footer/Footer.jsx'
-import Header from "../App";
+
 
 class About extends Component {
     render() {
         return (
-            <div>
-                <Header drawerClickHandler={this.drawerToggleClickHandler}/>
-                <h1>OM OSS</h1>
-                <Footer />
-
+            <div className='mainContainer'>
+            <div className='formContainer'>
+              <h2 className='header'>Om oss</h2>
             </div>
+          </div>
         );
     }
 }
--- src/components/startpage/StartPage.jsx
+++ src/components/startpage/StartPage.jsx
@@ -1,13 +1,21 @@
 import React, {Component} from 'react';
 import './StartPage.css';
+import logo from '../../images/logo.png';
+
 
 class StartPage extends Component {
     render () {
         return ( 
-          <div className='mainContainer'>
+          <div className='mainContainerStartPage'>
             <div className='formContainer'>
+              <div className='header'><img className="logo" src={logo} alt="logo"/></div>
+
               <h1 className='quote'>«A place to connect with other students»</h1>
+              
+              <a href='/LoginPage'>
               <button className="btn-logIn">Logg inn</button>
+              </a>
+
             <button className='btn-joinIceBreaker'>Join Ice Breaker</button>
             </div>
           </div>
