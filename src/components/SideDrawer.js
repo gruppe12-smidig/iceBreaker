4a5963887eec97c9efe1d882fba4fd255ef445a9
Karolis <karolis.pt@gmail.com>
2019-05-27 10:12:06 +0200

    Merge branch 'master' of /Users/Karolis/WebstormProjects/IceBreaker with conflicts.

--- src/components/SideDrawer.js
+++ src/components/SideDrawer.js
@@ -6,15 +6,15 @@ import './SideDrawer.css'
 class SideDrawer extends Component {
     render() {
         return(
-    <nav className="side-drawer">
+    <navlink className="side-drawer">
         <ul>
-            <li><Link to="/">Home</Link> </li>
+            <li><Link to="/">Home</Link></li>
             <li><Link to="/LoginPage">Login Page</Link></li>
             <li><Link to="/SignupPage">Sign up</Link></li>
             <li><Link to="/RegisterEventPage">New event</Link></li>
             <li><Link to="/Events">Events</Link></li>
         </ul>
-    </nav>
+    </navlink>
         );
     }
 }
