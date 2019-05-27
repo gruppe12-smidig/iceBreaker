206f878ad787490ae9131f90c624c652df67870e
Linda <lindae.osterberg@gmail.com>
2019-05-27 16:39:31 +0200

    event with chat

--- src/components/eventView/EventView.jsx
+++ src/components/eventView/EventView.jsx
@@ -3,6 +3,7 @@ import './EventView.css';
 import Footer from "../footer/Footer";
 import Header from '../header/Header';
 import floePeng from '../../images/floePeng.png';
+import peng from '../../images/penguin.png';
 
 
 import calender from '../../images/kalender-white.png';
@@ -23,7 +24,7 @@ class EventView extends Component {
                     <div className="mainContainer">
 
                         <div className='eventViewContainer'>
-                        <h2 className='subHeader' > Navn på Arr</h2>
+                        <h2 className='subHeader' > Kontegrupp 2</h2>
 
                     <div className='descriptionWrapper'>
 
@@ -57,6 +58,64 @@ class EventView extends Component {
                     <img className='floePeng' src={floePeng} alt="Image penguins" />
 
                         </div>
+
+                        <div className='userContainer'>
+                            <ul>
+                                <li className='user'> <img className='peng' src={peng} alt="Image penguin" /> <span>Anne Lien</span>   </li>
+                                <li className='user'> <img className='peng' src={peng} alt="Image penguin" /> <span>Per Olsen</span>  </li>
+                                <li className='user'> <img className='peng' src={peng} alt="Image penguin" /> <span>Johan Berg</span>  </li>
+                            </ul>
+                        </div>
+                        <div className='chatContainer'>
+
+                            <div className='post'>
+
+                                <div className='wrapperUserIcon'>
+                                    <div className='userIcon'></div>
+                                </div>
+
+                                <div className='wrapperMessage'>
+                                    <p className='message'>«Hei folkens! Har dere startet, hvor møtes vi?»</p>
+                                </div>
+                                
+
+                            </div>
+
+
+                            <div className='post'>
+
+                                <div className='wrapperUserIcon'>
+                                    <div className='userIcon'></div>
+                                </div>
+
+                                <div className='wrapperMessage'>
+                                    <p className='message'>«Hei folkens! Har dere startet, hvor møtes vi?»</p>
+                                </div>
+                                
+
+                            </div>
+
+
+                            <div className='post'>
+
+                                <div className='wrapperUserIcon'>
+                                    <div className='userIcon'></div>
+                                </div>
+
+                                <div className='wrapperMessage'>
+                                    <p className='message'>«Halo! Jeg sitter på Fjerdingen i morgen Onsdag fra 13:00. Dere kan kontakte meg på 4055789»</p>
+                                </div>
+                                
+
+                            </div>
+
+                        </div>
+                        <div className='chatFormWrapper'>
+
+                                <div className='chatForm'></div>
+                                <div className='chatBtn'></div>
+                        </div>
+                      
                     </div>
 
             <Footer/> 
