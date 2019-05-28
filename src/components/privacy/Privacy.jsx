5fdb9f9f2fad6859e920206b62fc3d7ac6b9d2fe
Sebastian <31272793+Slaskenx@users.noreply.github.com>
2019-05-28 09:32:13 +0200

    Privacy update

--- src/components/privacy/Privacy.jsx
+++ src/components/privacy/Privacy.jsx
@@ -12,12 +12,19 @@ class Privacy extends Component {
                 <h2 className='pageHeader'>Personvernerklæring</h2>
 
                 <div className='textWrapper'>
-                    <p>
+                    <p className='ingress'>
                     Lorem ipsum dolor sit amet 
                     consectetur adipisicing elit. Perspiciatis, aspernatur?
                     Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
                     Qui deleniti expedita, quaerat consequuntur illo consectetur 
                     modi nesciunt sunt voluptatibus praesentium.
+                    </p>
+                    <p className='mainArticle'>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                        Dolor repellendus quas aliquid adipisci perspiciatis unde quos doloribus 
+                        dignissimos quae, veritatis voluptas rem magnam. Neque, aliquid vel? 
+                        Non, consectetur! Saepe mollitia officiis sit eaque earum perspiciatis 
+                        ipsum quibusdam! Quos eos iste sint sapiente nobis autem sequi provident quidem harum id! Voluptates?
                     </p>
                 </div>
                 <Footer/>
