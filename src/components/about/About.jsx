3a4812ba86dcdd40eacb54a6ee2940907f7a871e
Linda <lindae.osterberg@gmail.com>
2019-06-14 10:40:36 +0200

    Update About.jsx

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -34,8 +34,7 @@ return (
             <div className='teamTextWrapper'>
                 <p className='contactUs-description'> Icebreaker er skapt av oss, en gruppe studenter ved Høyskolen Kristiania.
                 Vår visjon er å tilby en produkt som studenter er interessert i å bruke. Vi saknet en plass hvor du kan  
-                treffe nye studenter ved å arrangere eller delta på arrangementer som studiegrupper, kaffetreff eller fysiske aktivitet. 
-                Icebreaker er tenk å være et enkelt og lavterskeltilbud for å kunne møte andre som er interessert i det samme som deg, eller jobber mot samme eksamen. 
+                treffe nye studenter ved å arrangere eller delta på arrangementer som studiegrupper, kaffetreff eller fysiske aktivitet.  
                 Vi håper at du kommer like dette like mye som oss!
               </p>
             </div>
